5a746cf94b53723854f29fd91fc265ccfc786d53
Benjamin Anding <b.p.anding@gmail.com>
2022-03-01 13:12:48 -0600

    Initial Commit

---
+++ Antfarm/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Antfarm
+//
+//  Created by Ben Anding on 3/1/22.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
